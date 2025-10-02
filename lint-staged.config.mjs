52dce471274e379db159cd244e24f2c07b662b73
kurealnum <oscar.gaske.cs@gmail.com>
2025-10-02 16:48:14 -0400

    build: tests + formatting

---
+++ lint-staged.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  "*.ts": (stagedFiles) => [
+    `eslint .`,
+    `prettier --write ${stagedFiles.join(" ")}`,
+  ],
+  "*.tsx": (stagedFiles) => [
+    `eslint .`,
+    `prettier --write ${stagedFiles.join(" ")}`,
+  ],
+};
