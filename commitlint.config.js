3a7811734d7a2bfa4a19f1f9bf432656a5044bb3
Oscar <116363763+kurealnum@users.noreply.github.com>
2025-10-05 12:46:44 -0400

    docs: added user documentation and .prettierrc (#12)
    * docs: added user documentation and .prettierrc
    
    * fix: formatting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,26 @@
 // Copyright Todd LLC, All rights reserved.
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
+    'type-enum': [
       2,
-      "always",
+      'always',
       [
-        "build", // Changes that affect the build system
-        "ci", // Changes to CI configuration files and scripts
-        "docs", // Documentation only changes
-        "feat", // A new feature
-        "fix", // A bug fix
-        "perf", // A code change that improves performance
-        "refactor", // A code change that neither fixes a bug nor adds a feature
-        "style", // Changes that do not affect the meaning of the code
-        "test", // Adding missing tests or correcting existing tests
-        "chore", // Other changes that don't modify src or test files
+        'build', // Changes that affect the build system
+        'ci', // Changes to CI configuration files and scripts
+        'docs', // Documentation only changes
+        'feat', // A new feature
+        'fix', // A bug fix
+        'perf', // A code change that improves performance
+        'refactor', // A code change that neither fixes a bug nor adds a feature
+        'style', // Changes that do not affect the meaning of the code
+        'test', // Adding missing tests or correcting existing tests
+        'chore', // Other changes that don't modify src or test files
       ],
     ],
-    "subject-case": [2, "always", "lower-case"],
-    "subject-empty": [2, "never"],
-    "subject-full-stop": [2, "never", "."],
-    "header-max-length": [2, "always", 72],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
   },
 };
