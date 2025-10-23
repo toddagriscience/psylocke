7a8393eed935b8ea9239af34e908d20cbc0b72fb
kurealnum <oscar.gaske.cs@gmail.com>
2025-10-22 21:09:22 -0400

    feat: set up typescript properly + test route

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,18 @@
 // Copyright Todd LLC, All rights reserved.
+import router from './routes/test.ts';
 
-import { NextFunction, Request, Response } from 'express';
-
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
 
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.cwd(), 'public')));
+app.use(router);
 
-// catch 404 and forward to error handler
-app.use(function (req: Request, res: Response, next: NextFunction) {
-  next(createError(404));
-});
+app.listen(8080, () => console.log('hereiam'));
 
-// error handler
-app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+export default app;
